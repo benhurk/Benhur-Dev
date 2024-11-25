aeb28a1329c23fd56b27413c3b2433e0df2b0722
Ben Hur <benhurk2801@gmail.com>
2024-11-25 15:22:53 -0300

    Styles changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,26 +4,25 @@ import linkedIcon from '../../assets/linkedin-icon.png';
 
 export default function Header() {
     return (
-        <header className='relative w-screen max-w-[1920px] md:h-[396px] lg:h-[496px] h-[296px] bg-[url("/header-bg.gif")] bg-no-repeat bg-cover overflow-hidden
-                        before:bg-black before:absolute before:top-0 before:left-0 before:block before:size-full before:opacity-60'>
-
+        <header className='relative w-screen max-w-[1920px] lg:h-[550px] md:h-[400px] h-[300px] bg-no-repeat bg-cover overflow-hidden'>
             <div className="relative top-1/2 -translate-y-1/2">
-                <div className='glow glow-rgb mx-auto lg:mb-8 md:mb-6 mb-2 md:w-48 md:h-48 lg:w-56 lg:h-56 w-36 h-36 rounded-full'>
+                <div className='glow glow-rgb mx-auto lg:mb-8 md:mb-6 mb-2 lg:size-52 md:size-48 size-36 rounded-full shadow-2xl'>
                     <img className='w-full h-full object-cover object-center rounded-full' src='/pfp.jpg' />
                 </div>
-                <div className="text-center font-main text-white drop-shadow-4dark lg:mb-8 md:mb-6 mb-2">
-                    <h1 className='lg:text-4xl md:text-3xl text-2xl mb-1'>Bem Hur Küchler</h1>
-                    <h2 className="lg:text-xl text-lg">Desenvolvedor front-end</h2>
+                <div className="w-fit mx-auto md:p-1 p-0.5 lg:mb-8 md:mb-6 mb-2 bg-black shadow-xl">
+                    <h1 className='text-center md:text-4xl text-2xl font-bold'>
+                        {':\\Bem Hur Küchler>'}
+                    </h1>
                 </div>
-                <div className="flex gap-16 justify-center">
+                <div className="flex lg:gap-16 md:gap-10 gap-8 justify-center">
                     <a href="https://linkedin.com/in/bem-hur-kuchler" target='_blank' className='glow glow-sm glow-white-blue rounded-full'>
-                        <img className='lg:w-12 md:w-11 w-9' src={linkedIcon} alt="LinkedIn" />
+                        <img className='lg:w-12 md:w-11 w-8' src={linkedIcon} alt="LinkedIn" />
                     </a>
                     <a href="https://github.com/benhurk" target='_blank' className='glow glow-sm glow-white-blue rounded-full'>
-                        <img className='lg:w-12 md:w-11 w-9' src={gitIcon} alt="GitHub" />
+                        <img className='lg:w-12 md:w-11 w-8' src={gitIcon} alt="GitHub" />
                     </a>
                     <a href="" className='glow glow-sm glow-white-blue rounded-full'>
-                        <img className='lg:w-12 md:w-11 w-9' src={cvIcon} alt="CV" />
+                        <img className='lg:w-12 md:w-11 w-8' src={cvIcon} alt="CV" />
                     </a>
                 </div>
             </div>
