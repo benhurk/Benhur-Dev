4ee57291d8c8a198c19a437786515db864849b0f
Ben Hur <benhurk2801@gmail.com>
2024-12-06 14:18:17 -0300

    Created a component for the tech icons

---
+++ src/components/HoverIcon/index.tsx
@@ -0,0 +1,16 @@
+type Props = {
+    img: string;
+    text: string;
+    className?: string;
+}
+
+export default function HoverIcon({img, text, className}: Props) {
+    return (
+        <div className={`${className ? className : ''} relative group hover:-translate-y-2 transition-transform duration-300 ease-in-out`}>
+            <img src={img} alt={text} />
+            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
+                {text}
+            </span>
+        </div>
+    )
+}
