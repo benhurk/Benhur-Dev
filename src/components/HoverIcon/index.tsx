1e6d459a14658b80a175e6d7c3bf4faa75df88c0
Ben Hur <benhurk2801@gmail.com>
2024-12-14 15:43:57 -0300

    Adjusted how much the icon moves up

--- src/components/HoverIcon/index.tsx
+++ src/components/HoverIcon/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function HoverIcon({img, text, className}: Props) {
     return (
-        <div className={`${className ? className : ''} relative group hover:-translate-y-2 transition-transform duration-300 ease-in-out`}>
+        <div className={`${className ? className : ''} relative group hover:-translate-y-1 transition-transform duration-300 ease-in-out`}>
             <img src={img} alt={text} />
             <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
                 {text}
