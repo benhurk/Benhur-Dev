84700b8519a6de289400945a1318f5b242e53419
Ben Hur <benhurk2801@gmail.com>
2024-12-16 18:00:50 -0300

    Fixed the file name and made some styling changes

--- src/components/TimePoint/index.tsx
+++ src/components/TimePoint/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function TimePoint({time, title, tooltip}: Props) {
     return (
-        <div className="relative group w-32">
+        <div className="relative group cursor-help w-32">
             <div className="flex flex-col gap-4 items-center">
                 <div className="w-8 h-8 animate-spin-slow glow glow-sm glow-rgb z-10">
                     <div className="size-full bg-slate-900" />
@@ -20,7 +20,7 @@ export default function TimePoint({time, title, tooltip}: Props) {
             </div>
             {
                 tooltip &&
-                <div className="absolute bottom-[115%] left-1/2 -translate-x-1/2 w-80 p-3 z-20 bg-gradient-to-br from-slate-100 to-white text-slate-700
+                <div className="absolute bottom-[115%] left-1/2 -translate-x-1/2 w-80 p-3 z-20 bg-gradient-to-br from-slate-100 to-white text-dark
                                 opacity-0 group-hover:opacity-100 group-hover:-translate-y-1.5 transition-all duration-300 ease-in-out pointer-events-none">
                     {tooltip}
                 </div>
